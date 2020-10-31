5b9db0999163c7c7e75b76bc764670d2c13bc533
James Jones <atari@theinnocuous.com>
2020-10-31 09:02:09 +0000

    test2.sh: Add serial EEPROM and 6MB flash coverage
    test2.sh will now verify 6MB flashing and readback
    work, running Cyrano's Bad Apple demo to do so.
    This doesn't necessarily verify the HW more than
    flashing two 4MB roms does, but it does verify the
    functionality in the BIOS and JCP.
    
    It will also verify the serial EEPROMs can both be
    programmed, toggled on/off, and read back on Rev.5
    boards (BIOS >= 4.0.0).
    
    Added the following files to .gitignore:
    
    test.bin     - Temporary file created by test2.sh
                   when it reads back a cartridge from
                   the skunkboard's flash for
                   verification. NOT temp.bin as prior
                   change claimed.
    
    test.e2p     - Temporary file created by test2.sh
                   when it reads back the skunkboard
                   serial EEPROM content for
                   verification.
    
    e2p.log      - Temporary file created by test2.sh
                   to parse the output of jcp when
                   manipulating the serial EEPROM.
    
    badapple.rom - The Bad Apple skunk 6MB ROM. Used
                   by test2.sh to verify 6MB flashing.
                   Must be supplied by the user,
                   available here, 'Bad Apple v7.rom':
    
      https://atariage.com/forums/topic/273595-bad-apple

--- test2.sh
+++ test2.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -f e2p.log ]; then
+	rm e2p.log
+fi
+
+if [ -f test.e2p ]; then
+	rm test.e2p
+fi
+
 if [ -f test.bin ]; then
 	rm test.bin
 fi
@@ -11,6 +19,76 @@ fi
 failed() {
 	echo -------------- FAILED ---------------
 	exit 1
+}
+
+e2ptests() {
+	echo
+	echo "*******************************************"
+	echo "*******************************************"
+	echo "****                                   ****"
+	echo "****    BEGIN SERIAL EEPROM TESTING    ****"
+	echo "****                                   ****"
+	echo "*******************************************"
+	echo "*******************************************"
+	echo
+	
+	echo .
+	echo "Write random data to EEPROM 1 (93C46)"
+	echo .
+	jcp -k 1
+	jcp -p rand-128.e2p |tee e2p.log
+	grep 'Detected 93C46' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	
+	echo .
+	echo "Write random data to EEPROM 2 (93C86)"
+	echo .
+	jcp -k 2
+	jcp -p rand-2048.e2p |tee e2p.log
+	grep 'Detected 93C86' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	
+	echo .
+	echo Try to write data while both EEPROMs are disabled
+	echo .
+	jcp -k 0
+	jcp -p rand-128.e2p | tee > e2p.log
+	grep 'No supported serial EEPROM' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	
+	echo .
+	echo "Read data back from EEPROM 1 (93C46) and compare to data written"
+	echo .
+	jcp -k 1
+	jcp -g test.e2p |tee e2p.log
+	grep 'Detected 93C46' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	echo Diff the files...
+	diff --binary test.e2p rand-128.e2p
+	if [ $? -ne 0 ]; then failed; fi
+	
+	echo .
+	echo "Read data back from EEPROM 2 (93C86) and compare to data written"
+	echo .
+	jcp -k 2
+	jcp -g test.e2p |tee e2p.log
+	grep 'Detected 93C86' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	echo Diff the files...
+	diff --binary test.e2p rand-2048.e2p
+	if [ $? -ne 0 ]; then failed; fi
+	
+	echo .
+	echo Clear both EEPROM banks
+	echo .
+	jcp -k 1
+	jcp -p ffff.e2p |tee e2p.log
+	grep 'Detected 93C46' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
+	jcp -k 2
+	jcp -p ffff.e2p |tee e2p.log
+	grep 'Detected 93C86' e2p.log>/dev/null
+	if [ $? -ne 0 ]; then failed; fi
 }
 
 jcp -r
@@ -139,14 +217,47 @@ fi
 sleep 3
 
 echo .
+echo Try a 6MB game...
+echo .
+jcp -6f badapple.rom
+echo Letting it run for 4 minutes 10 seconds
+sleep 250
+jcp -r
+sleep 3
+
+echo .
+echo Now dump 6MB back...
+echo .
+jcp -6d test.bin
+echo Diff the files...
+diff --binary badapple.rom test.bin
+if [ $? -ne 0 ]; then
+	failed
+fi
+sleep 3
+
+echo .
 echo Write the zeros back to bank 2
 echo .
 jcp -2f zerocart.bin
 sleep 3
 jcp -r
+sleep 3
+
+echo .
+echo Write the zeros back to bank 1
+echo .
+jcp -f zerocart.bin
+sleep 3
+jcp -r
+sleep 3
+
+if [ ${BIOS_MAJOR} -ge 4 ]; then
+	e2ptests
+fi
 
 echo .
 echo !!!!!SUCCESS!!!!!
-rm bios.bin test.bin
+rm -f bios.bin test.bin test.e2p e2p.log
 
 exit 0
